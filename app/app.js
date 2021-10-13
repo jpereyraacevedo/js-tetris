12119e33ed05b1ff02dd65ed9d319e3b18731217
jpereyraacevedo <jpereyraacevedo@gmail.com>
2021-10-13 02:03:17 -0300

    first commit

---
+++ app/app.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll(".grid div"))
+    const ScoresDisplay = document.querySelector("#score")
+    const StartButton = document.querySelector("#start-button")
+    const width = 10
+
+
+
+
+
+
+
+})
